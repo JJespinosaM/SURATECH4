cd07549aeb28143d0ddef827fb4e8abd2683afb3
jjrojasan <jehoverr@gmail.com>
2024-09-17 15:03:16 -0500

    pegar

---
+++ src/main/java/com/example/servidor_suratech_4/MODELOS/PACIENTES.java
@@ -0,0 +1,18 @@
+package com.example.servidor_suratech_4.MODELOS;
+
+import java.time.LocalDate;
+
+public class PACIENTES {
+
+    private Integer id;
+    private String nombres;
+    private Integer edad;
+    private LocalDate fechaNacimiento;
+    private String telefono;
+    private Integer copago;
+    private String email;
+    private String direccion;
+    private String ips;
+    private String medicoFamilia;
+
+}
